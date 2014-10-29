f07fbefb13d30dc220cd42410f3222d6d6a3262c
waidmann <ben100691@gmail.com>
2014-10-29 13:04:15 +0000

    retrieves financial information and parses

--- Server/API_Test2.py
+++ Server/API_Test2.py
@@ -1,11 +1,26 @@
-import urllib2
+# Queries through YQL and parses data, will mainly focus on yahoo.finance for
+# the wealth tool
+# TODO
+# implement caching to minimise requests
+# clean up layout for easy use through the result of the application
+# add further requests
+# perhaps get the latest data / updated data
+# historical information
+
 import urllib
 import json
+import httplib
 
-baseurl = "https://query.yahooapis.com/v1/public/yql?"
-yql_query = "select wind from weather.forecast where woeid=2460286"
-yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-result = urllib2.urlopen(yql_url).read()
-data = json.loads(result)
+# API urls
+PUBLIC_API_URL = 'http://query.yahooapis.com/v1/public/yql'
+DATABASE_URL = 'store://datatables.org/alltableswithkeys'
 
-print data['query']['results']
+
+def executeYQLQUERY(yql):
+        conn = httplib.HTTPConnection('query.yahooapis.com')
+        queryString = urllib.urlencode({'q': yql, 'format': 'json',
+                                        'env': DATABASE_URL})
+        conn.request('GET', PUBLIC_API_URL + '?' + queryString)
+        return json.loads(conn.getresponse().read())
+
+print executeYQLQUERY("select * from yahoo.finance.quant where symbol=\"YHOO\"")
