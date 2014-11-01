6f2b2dc6baaed86e7377741a7523c48c3ffaece3
waidmann <ben100691@gmail.com>
2014-11-01 20:05:48 +0000

    Error handling

--- Server/API_Test2.py
+++ Server/API_Test2.py
@@ -6,6 +6,7 @@
 # add further requests
 # perhaps get the latest data / updated data
 # historical information
+# document
 
 import urllib
 import json
@@ -14,11 +15,13 @@ import httplib
 # API urls
 PUBLIC_API_URL = 'http://query.yahooapis.com/v1/public/yql'
 DATABASE_URL = 'store://datatables.org/alltableswithkeys'
+
 # API Tables (mostly community tables)
 YQL_TABLES = {'stocks': 'yahoo.finance.stocks',
               'xchange': 'yahoo.finance.xchange'}  # For currency exchange
 
 
+# Create one connection per query, caching a possible future implementation
 def executeYQLQUERY(yql):
         conn = httplib.HTTPConnection('query.yahooapis.com')
         queryString = urllib.urlencode({'q': yql, 'format': 'json',
@@ -27,6 +30,7 @@ def executeYQLQUERY(yql):
         return json.loads(conn.getresponse().read())
 
 
+# Catch exceptions with queries, query failures usually have the word 'error'
 class QueryError(Exception):
 
     def __init__(self, value):
@@ -35,4 +39,12 @@ class QueryError(Exception):
     def __str__(self):
             return repr(self.value)
 
-print executeYQLQUERY("select * from %(xchange)s where pair in (\"EURUSD\",\"GBPUSD\")" % YQL_TABLES)
+
+def _is_query_valid(response):
+    if 'error' in response:
+        raise QueryError('YQL query failed: "%(error)s %(description)s"'
+                         % response)
+
+
+print executeYQLQUERY("select * from %(xchange)s where pair in \
+                      (\"EURUSD\",\"GBPUSD\")" % YQL_TABLES)
