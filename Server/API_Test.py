26bb12d9e22ff4f04fa52066bee8ee613c9cbddd
burgundybagder <cajturner@gmail.com>
2014-11-02 21:44:29 +0000

    updated API_Test.py to a function that takes the query as input

--- Server/API_Test.py
+++ Server/API_Test.py
@@ -1,15 +1,17 @@
 import urllib.request as urlrequest
 import urllib.parse as urlparse 
-import json
+import json, sys
 
-BASE_URL = "https://query.yahooapis.com/v1/public/yql?"
-CALLBACK = "http://www.metissoftware.co.uk"
-ENV = "store://datatables.org/alltableswithkeys"
+def query_api(yql_query):
+	BASE_URL = "https://query.yahooapis.com/v1/public/yql?"
+	CALLBACK = "http://www.metissoftware.co.uk"
+	ENV = "store://datatables.org/alltableswithkeys"
 
-yql_query = 'select symbol from yahoo.finance.quote where symbol in ("YHOO","AAPL","GOOG","MSFT")'
-yql_url = BASE_URL + urlparse.urlencode({'q':yql_query, 'env': ENV})  + "&format=json"
+	yql_url = BASE_URL + urlparse.urlencode({'q':yql_query, 'env': ENV})  + "&format=json"
 
-result = urlrequest.urlopen(yql_url).read()
-response = json.loads(result.decode('utf8'))
+	result = urlrequest.urlopen(yql_url).read()
+	response = json.loads(result.decode('utf8'))
+	return response
 
-print(response['query']['results'])
+print(query_api('select * from yahoo.finance.quote where symbol in ("YHOO","AAPL","GOOG","MSFT")'))
+	
