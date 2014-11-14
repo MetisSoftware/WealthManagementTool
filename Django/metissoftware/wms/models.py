a33d5d1b2b495e4b74b4075984faebf303731d45
Ben <ben@waidmann.home>
2014-11-14 17:15:50 +0000

    Added string representation of clients

--- Django/metissoftware/wms/models.py
+++ Django/metissoftware/wms/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Client(models.Model):
     first_name = models.CharField(max_length=64)
     surname = models.CharField(max_length=64)
     email = models.EmailField()
+
+    def __str__(self):  # __unicode__ if using python 2
+        return self.first_name
