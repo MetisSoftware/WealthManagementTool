891d127f92de2dda563dae9135c680cc63329dc3
burgundybagder <cajturner@gmail.com>
2014-11-17 18:33:20 +0000

    updated unit test for client model

--- Django/metissoftware/metissoftware/urls.py
+++ Django/metissoftware/metissoftware/urls.py
@@ -6,6 +6,4 @@ urlpatterns = patterns('wms.views',
     url(r'^$', 'index', name='index'),
     url(r'^clients/', 'print_client', name='print_client'),
     url(r'^new_client.html', 'new_client', name='new_client'),
-
-
 )
