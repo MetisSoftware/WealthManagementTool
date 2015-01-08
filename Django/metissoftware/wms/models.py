b7fa2cda7b7789f92ef4e0c694fc590e9e396807
waidmann <ben100691@gmail.com>
2015-01-08 21:24:32 +0000

    NI validation

--- Django/metissoftware/wms/models.py
+++ Django/metissoftware/wms/models.py
@@ -9,7 +9,9 @@ class User(models.Model):
     first_name = models.CharField(max_length=64, default="**DEFAULT**")
     surname = models.CharField(max_length=64, default="**DEFAULT**")
     dob = models.DateField(default="1990-01-01")
-    ni_number = models.CharField(max_length=9, default="DEFAULT", primary_key=True)
+    ni_regex = RegexValidator(regex=r'^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)$', message="Must be in the format: 'AA999999A', restrictions to characters apply'")
+    ni_number = models.CharField(max_length=9, validators=[ni_regex], primary_key=True)
+    #ni_number = models.CharField(max_length=9, default="DEFAULT", primary_key=True)
     email = models.EmailField(max_length=64, default="**DEFAULT**")
 
     class Meta:
@@ -17,9 +19,9 @@ class User(models.Model):
 
 
 class FA(User):
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 9 digits allowed.")
-    mob_number = models.CharField(max_length=11, null=True)
-    off_number = models.CharField(max_length=11, null=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Must be entered in the format: '+999999999'.")
+    mob_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
+    off_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
     password = models.CharField(max_length=64, default="**DEFAULT**")
 
     def __str__(self):
@@ -31,8 +33,6 @@ class Client(User):
     middle_name = models.CharField(max_length=64, null=True)
     home_phone = models.CharField(max_length=11, validators=[phone_regex], blank=True)
     mob_phone = models.CharField(max_length=11, validators=[phone_regex], blank=True)
-    #home_phone = models.CharField(max_length=11, null=True)
-    #mob_phone = models.CharField(max_length=11, null=True)
     cash = models.DecimalField(max_digits=20,decimal_places=2)
     fa = models.ForeignKey(FA)
 
