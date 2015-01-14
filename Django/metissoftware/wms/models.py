6cb60befe36b5034e964a1a5fc584ed039e49eb7
waidmann <ben100691@gmail.com>
2015-01-14 21:58:29 +0000

    remodelled FA as user object

--- Django/metissoftware/wms/models.py
+++ Django/metissoftware/wms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.forms import ModelForm, TextInput
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractBaseUser
 
 
 # Create your models here.
@@ -11,21 +12,31 @@ class User(models.Model):
     dob = models.DateField(default="1990-01-01")
     ni_regex = RegexValidator(regex=r'^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)$', message="Must be in the format: 'AA999999A', restrictions to characters apply'")
     ni_number = models.CharField(max_length=9, validators=[ni_regex], primary_key=True)
-    #ni_number = models.CharField(max_length=9, default="DEFAULT", primary_key=True)
     email = models.EmailField(max_length=64, default="**DEFAULT**")
 
     class Meta:
         abstract = True
 
 
-class FA(User):
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Must be entered in the format: '+999999999'.")
-    mob_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
-    off_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
-    password = models.CharField(max_length=64, default="**DEFAULT**")
+#class FA(User):
+    #phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Must be entered in the format: '+999999999'.")
+    #mob_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
+    #off_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
+    #password = models.CharField(max_length=64, default="**DEFAULT**")
 
-    def __str__(self):
-        return self.surname+" - "+self.ni_number
+    #def __str__(self):
+        #return self.surname+" - "+self.ni_number
+
+class FA(AbstractBaseUser):
+    email = models.EmailField(max_length=64, unique=True)
+    first_name = models.CharField(max_length=64, default="**DEFAULT**")
+    surname = models.CharField(max_length=64, default="**DEFAULT**")
+    dob = models.DateField(default="1990-01-01")
+    ni_regex = RegexValidator(regex=r'^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)$', message="Must be in the format: 'AA999999A', restrictions to characters apply'")
+    ni_number = models.CharField(max_length=9, validators=[ni_regex], primary_key=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'surname', 'dob', 'ni_number']
+
 
 
 class Client(User):
