6e73299752dad485f2f61dc572e14653b101ab38
waidmann <ben100691@gmail.com>
2015-01-19 13:17:46 +0000

    adding superuser with permissions

--- Django/metissoftware/wms/models.py
+++ Django/metissoftware/wms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.forms import ModelForm, TextInput
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 
 # Create your models here.
@@ -19,8 +20,8 @@ class User(models.Model):
 
 
 class FAUserManager(BaseUserManager):
-    def create_user(self, email, first_name, surname, is_superuser,
-                    dob, ni_number, password=None, **extra_fields):
+    def create_user(self, email, first_name, surname,
+                    dob, ni_number, password, **extra_fields):
         if not email:
             raise ValueError('users must have an email address')
         if not first_name:
@@ -33,19 +34,17 @@ class FAUserManager(BaseUserManager):
             raise ValueError('users must have a national insurance number')
 
         user = self.model(email=email, first_name=first_name,
-                          surname=surname, dob=dob, ni_number=ni_number,
-                          is_superuser=is_superuser)
+                          surname=surname, dob=dob, ni_number=ni_number)
 
         user.set_password(password)
-        user.normalize_email(email)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, first_name, surname,
-                         dob, ni_number, password=None, **extra_fields):
+                         dob, ni_number, password, **extra_fields):
 
-        user = self.create_user(email, first_name, surname, ni_number,
-                                password, **extra_fields)
+        user = self.create_user(email, first_name, surname, dob,
+                                ni_number, password, **extra_fields)
 
         user.is_superuser = True
         user.save(using=self._db)
