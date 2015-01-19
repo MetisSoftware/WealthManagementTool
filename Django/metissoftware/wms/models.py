97087ef633f032892b9df4dcbc3be4a8425d5891
waidmann <ben100691@gmail.com>
2015-01-19 11:37:03 +0000

    added manager to custom user

--- Django/metissoftware/wms/models.py
+++ Django/metissoftware/wms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.forms import ModelForm, TextInput
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
@@ -18,38 +18,70 @@ class User(models.Model):
         abstract = True
 
 
-#class FA(User):
-    #phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Must be entered in the format: '+999999999'.")
-    #mob_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
-    #off_number = models.CharField(validators=[phone_regex], max_length=9, null=True)
-    #password = models.CharField(max_length=64, default="**DEFAULT**")
+class FAUserManager(BaseUserManager):
+    def create_user(self, email, first_name, surname, is_superuser,
+                    dob, ni_number, password=None, **extra_fields):
+        if not email:
+            raise ValueError('users must have an email address')
+        if not first_name:
+            raise ValueError('users must have a first name')
+        if not surname:
+            raise ValueError('users must have a surname')
+        if not dob:
+            raise ValueError('users must have a dob')
+        if not ni_number:
+            raise ValueError('users must have a national insurance number')
 
-    #def __str__(self):
-        #return self.surname+" - "+self.ni_number
+        user = self.model(email=email, first_name=first_name,
+                          surname=surname, dob=dob, ni_number=ni_number,
+                          is_superuser=is_superuser)
 
-class FA(AbstractBaseUser):
+        user.set_password(password)
+        user.normalize_email(email)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, surname,
+                         dob, ni_number, password=None, **extra_fields):
+
+        user = self.create_user(email, first_name, surname, ni_number,
+                                password, **extra_fields)
+
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class FA(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=64, unique=True)
     first_name = models.CharField(max_length=64, default="**DEFAULT**")
     surname = models.CharField(max_length=64, default="**DEFAULT**")
     dob = models.DateField(default="1990-01-01")
     ni_regex = RegexValidator(regex=r'^(?!BG)(?!GB)(?!NK)(?!KN)(?!TN)(?!NT)(?!ZZ)(?:[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z])(?:\s*\d\s*){6}([A-D]|\s)$', message="Must be in the format: 'AA999999A', restrictions to characters apply'")
-    ni_number = models.CharField(max_length=9, validators=[ni_regex], primary_key=True)
+    ni_number = models.CharField(
+        max_length=9, validators=[ni_regex], primary_key=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'surname', 'dob', 'ni_number']
 
+    objects = FAUserManager()
 
 
 class Client(User):
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 9 digits allowed.")
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Not a valid phone number. Up to 9 digits allowed.")
     middle_name = models.CharField(max_length=64, null=True)
-    home_phone = models.CharField(max_length=11, validators=[phone_regex], blank=True)
-    mob_phone = models.CharField(max_length=11, validators=[phone_regex], blank=True)
-    cash = models.DecimalField(max_digits=20,decimal_places=2)
+    home_phone = models.CharField(
+        max_length=11, validators=[phone_regex], blank=True
+    )
+    mob_phone = models.CharField(
+        max_length=11, validators=[phone_regex], blank=True
+    )
+    cash = models.DecimalField(max_digits=20, decimal_places=2)
     fa = models.ForeignKey(FA)
 
     def __str__(self):
-        return self.surname+" - "+self.ni_number
-
+        return self.surname + " - " + self.ni_number
 
 
 class Market(models.Model):
@@ -57,25 +89,29 @@ class Market(models.Model):
     full_name = models.CharField(max_length=64)
 
     def __str__(self):
-        return  self.name
+        return self.name
+
 
 class Stock(models.Model):
-    symbol = models.CharField(primary_key=True,max_length=5)
+    symbol = models.CharField(primary_key=True, max_length=5)
     company = models.CharField(max_length=64)
     market = models.ForeignKey(Market)
 
     def __str__(self):
         return self.symbol
 
+
 class Share(models.Model):
     owner = models.ForeignKey(Client)
-    buy_date = models.DateField(default = "1990-01-01")
+    buy_date = models.DateField(default="1990-01-01")
     amount = models.IntegerField()
-    price = models.DecimalField(max_digits=20,decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
     stock = models.ForeignKey(Stock)
 
     def __str__(self):
-        return self.owner.surname + " " + self.owner.ni_number+" - "+self.stock.symbol
+        return self.owner.surname + " \
+               " + self.owner.ni_number + " - " + self.stock.symbol
+
 
 class ClientForm(ModelForm):
     class Meta:
@@ -83,7 +119,9 @@ class ClientForm(ModelForm):
         fields = ['first_name', 'middle_name', 'surname', 'email',
                   'mob_phone', 'home_phone', 'dob', 'ni_number', 'fa', 'cash']
         widgets = {
-            'cash': TextInput(attrs={'placeholder': '0.00', 'cols': '1', 'rows': '1'}),
+            'cash': TextInput(
+                attrs={'placeholder': '0.00', 'cols': '1', 'rows': '1'}
+            ),
         }
 
     def __str__(self):
