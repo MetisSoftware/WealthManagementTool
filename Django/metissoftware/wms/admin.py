a8b36de14272e705289e481cfa2aaa792dec0a8e
waidmann <ben100691@gmail.com>
2015-01-28 20:53:41 +0000

    admin interface working with add user

--- Django/metissoftware/wms/admin.py
+++ Django/metissoftware/wms/admin.py
@@ -1,10 +1,61 @@
 from django.contrib import admin
 from wms.models import Client, FA, Market, Share, Stock
+from django import forms
+from django.contrib.auth.admin import UserAdmin
+
+
+class FACreationForm(forms.ModelForm):
+    # For creating FAs
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = FA
+        fields = ('first_name', 'surname', 'email', 'dob',
+                  'ni_number', 'is_staff', 'is_admin', 'is_active'
+                  )
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        if not password:
+            raise forms.ValidationError("Empty password")
+        return password
+
+
+    def save(self, commit=True):
+        user = super(FACreationForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
+
+
+class FAAdmin(UserAdmin):
+    add_form = FACreationForm
+
+    list_display = ('email', 'dob', 'is_admin')
+    fieldsets = (
+        (None, {'fields': ('first_name', 'surname', 'ni_number',
+                           'is_staff','is_admin', 'is_active')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'surname', 'ni_number',
+                       'email', 'dob', 'is_staff', 'is_admin', 'password')
+        }),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
 
 
 # Register your models here.
 admin.site.register(Client)
-admin.site.register(FA)
+admin.site.register(FA, FAAdmin)
 admin.site.register(Market)
 admin.site.register(Share)
 admin.site.register(Stock)
